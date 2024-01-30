4ceb044680c0b4aa6df5366c7d3d63cb521fe285
ysoamerr <158029116+ysoamerr@users.noreply.github.com>
2024-01-30 15:10:33 +0300

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Mody(object):
+    ELHYBA = os.environ.get("ELHYBA", "")
+    
+    OWNER = os.environ.get("OWNER", "6396782078")
+    
+    ID_BOT = int(os.environ.get("ID_BOT", ""))
+    
+    USER_BOT = os.environ.get("USER_BOT", "")
+    
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", ""))
+    
+    API_ID = os.environ.get("API_ID", "12453602")
+    
+    API_HASH = os.environ.get("API_HASH", "96e7a1b8d3fb5bd8cf35f2db2e34daa4")
+    
+
